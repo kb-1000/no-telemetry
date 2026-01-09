d0699a61d16a5fe71832ce1b971335b1bea190c2
kb1000 <kaeptmblaubaer1000@gmail.com>
2026-01-09 09:30:46 +0100

    Support refactored NeoForge FMLLoader

--- src/main/java/de/kb1000/notelemetry/NoTelemetryNeoForgeMixinConfigPlugin.java
+++ src/main/java/de/kb1000/notelemetry/NoTelemetryNeoForgeMixinConfigPlugin.java
@@ -9,9 +9,14 @@
 package de.kb1000.notelemetry;
 
 import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.fml.loading.VersionInfo;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
 import org.apache.maven.artifact.versioning.VersionRange;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 
 public class NoTelemetryNeoForgeMixinConfigPlugin extends NoTelemetryAbstractMixinConfigPlugin {
     @Override
@@ -29,10 +34,31 @@ public class NoTelemetryNeoForgeMixinConfigPlugin extends NoTelemetryAbstractMix
         return true;
     }
 
+    private static final MethodHandle getVersionInfo;
+
+    static {
+        MethodHandle methodHandle;
+        try {
+            //noinspection JavaLangInvokeHandleSignature
+            methodHandle = MethodHandles.lookup().findStatic(FMLLoader.class, "versionInfo", MethodType.methodType(VersionInfo.class));
+        } catch (NoSuchMethodException e) {
+            try {
+                methodHandle = MethodHandles.filterReturnValue(MethodHandles.lookup().findStatic(FMLLoader.class, "getCurrent", MethodType.methodType(FMLLoader.class)), MethodHandles.lookup().findVirtual(FMLLoader.class, "getVersionInfo", MethodType.methodType(VersionInfo.class)));
+            } catch (NoSuchMethodException | IllegalAccessException ex) {
+                throw new RuntimeException(ex);
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        getVersionInfo = methodHandle;
+    }
+
     protected boolean minecraftNewerThan(String version) {
         try {
-            return VersionRange.createFromVersionSpec("[" + version + ",)").containsVersion(new DefaultArtifactVersion(FMLLoader.versionInfo().mcVersion()));
-        } catch (InvalidVersionSpecificationException e) {
+            return VersionRange.createFromVersionSpec("[" + version + ",)").containsVersion(new DefaultArtifactVersion(((VersionInfo) getVersionInfo.invoke()).mcVersion()));
+        } catch (Error | RuntimeException e) {
+            throw e;
+        } catch (Throwable e) {
             throw new RuntimeException(e);
         }
     }
